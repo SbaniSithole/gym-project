1b9f170014d1f6e482e3c6b45aba30e979302987
Sbani3010 <firebasedev20@gmail.com>
2023-08-25 00:32:55 +0200

    first commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,12 @@ type Data = {
   name: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  const resp=await fetch("http://localhost:8084/movie/all");
+  const data=await resp.json();
   res.status(200).json({ name: 'John Doe' })
 }
